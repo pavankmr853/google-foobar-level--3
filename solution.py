284c8a009bc284e5e0cac2f31f625b435c4d19f9
PARDHU SETTY <pavankmr853@gmail.com>
2020-05-15 22:49:39 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,15 @@
+def solution(n):
+    # Your code here
+  n = int(n)
+  steps = 0
+  
+  while n > 1:
+    # if n is even, divide by 2 using bit manipulation
+    if n & 1 == 0:
+      n >>= 1
+    else:
+      # Use bit manipulation to create as many 0 from LSB as possible
+      n = (n - 1) if (n == 3 or n % 4 == 1) else (n + 1)
+
+    steps += 1
+  return steps
